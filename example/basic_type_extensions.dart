2c671ed08f2796d677c2b4c5617fd53270de36c8
Filip Hracek <filip.hracek@gmail.com>
2019-11-07 10:17:56 -0800

    “Improve” basic type extensions

--- example/basic_type_extensions.dart
+++ example/basic_type_extensions.dart
@@ -1,8 +1,40 @@
+import 'dart:math' show pi;
+
 extension Silly on String {
-	void scream() => print("**** ${this.toUpperCase()} ****");
+  void scream() => print(toUpperCase());
+}
+
+extension Sillier on int {
+  void times(Function f) {
+    for (int i = 0; i < this; i++) {
+      f();
+    }
+  }
+}
+
+extension Silliest on Object {
+  int nah() => hashCode + 42;
+  String stop(String x) => toString() + x;
+  Type please() => runtimeType;
+  bool notAgain(Object other) => this != other;
+  // ...
 }
 
 void main() {
-	var warning = "Extensions are here!";
-	warning.scream(); // prints "**** EXTENSIONS ARE HERE! ****"
+  var warning = "Extensions are here!";
+  warning.scream(); // prints "EXTENSIONS ARE HERE!"
+
+  // Extension methods ¯\_(ツ)_/¯
+  5.times(() => print('hello'));
+
+  print(null.stop('!!!')); // prints null!!!
+
+  // Without extension methods:
+  print('Extension methods are here!'.toUpperCase());
+
+  for (var i = 0; i < 3; i++) {
+    print('Hurray!');
+  }
+
+  print('π = $pi');
 }
